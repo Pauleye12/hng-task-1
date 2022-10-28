711915b25f5a38ac1581dc2d28fe0730a811337c
pauleye <orepaul12@gmail.com>
2022-10-28 10:48:34 +0100

    added subtext and title tag

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Link(props) {
   return (
     <div className="link">
-      <a href={props.content.btn_link}>{props.content.btn_label}</a>
+      <a href={props.content.btn_link} title={props.content.sub}>
+        {props.content.btn_label}{" "}
+      </a>
     </div>
   );
 }
--- src/components/renderLink.jsx
+++ src/components/renderLink.jsx
@@ -7,34 +7,40 @@ function RenderLink() {
     {
       Id: "twitter",
       btn_link: "https://twitter.com/pauleye12_/",
-      btn_label: "Twitter Link"
+      btn_label: "Twitter Link",
+      sub: ''
     },
     {
       Id: "btn_zuri",
       btn_link: "https://training.zuri.team/",
-      btn_label: "Zuri Team"
+      btn_label: "Zuri Team",
+      sub: ''
     },
     {
       Id: "books",
       btn_link: "https://books.zuri.team/",
-      btn_label: "Zuri Books"
+      btn_label: "Zuri Books",
+      sub: "this is where you find books about design and coding",
     },
     {
       Id: "book_python",
       btn_link:
-        "https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>",
-      btn_label: "Python Books"
+        "https://books.zuri.team/python-for-beginners?ref_id=<adeleye oreoluwa>",
+      btn_label: "Python Books",
+      sub: "ome and get your books at affordable prices",
     },
     {
       Id: "pitch",
       btn_link: "https://background.zuri.team/",
-      btn_label: "Background Check for Coders"
+      btn_label: "Background Check for Coders",
+      sub: "I do good background check on coders to offer their services to you",
     },
     {
       Id: "book_design",
       btn_link: "https://books.zuri.team/design-rules",
-      btn_label: "Design Books"
-    }
+      btn_label: "Design Books",
+      sub: "zuri offers free design book for all aspiring ui/ux designers",
+    },
   ]);
 
     return <div className="links">
